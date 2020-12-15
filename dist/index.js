6c4d6576490eb6a156c051733598eecf02e5d4ca
Jago (C. Santiago Melon) <jago@kairosds.com>
2020-12-15 18:27:25 +0100

    Distributed files are now minified

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,1 @@
-export { default as en } from './en';
-export { default as es } from './es';
-export { default as de } from './de';
-export { default as fr } from './fr';
-export { default as pt } from './pt';
-export { default as it } from './it';
+export{default as en}from"./en";export{default as es}from"./es";export{default as de}from"./de";export{default as fr}from"./fr";export{default as pt}from"./pt";export{default as it}from"./it";
